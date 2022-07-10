93d88968aa9afc49d6e2ec85a865296232dc025e
Linoy Yarkoni <linoyarkoni20@gmail.com>
2022-07-10 23:13:23 +0300

    add activity
    login
    signin

--- app/src/main/java/com/example/gymtracker/SignInActivity.java
+++ app/src/main/java/com/example/gymtracker/SignInActivity.java
@@ -3,6 +3,7 @@ package com.example.gymtracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+    }
+
+    public void enableGetStartedButton(View view) {
     }
 }
