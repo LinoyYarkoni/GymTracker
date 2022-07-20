619433d8179cb07101056ad482e5e2cb2498eb03
Linoy Yarkoni <linoyarkoni20@gmail.com>
2022-07-20 11:41:53 +0300

    chosen list item

--- app/src/main/java/com/example/gymtracker/ExercisesActivity.java
+++ app/src/main/java/com/example/gymtracker/ExercisesActivity.java
@@ -1,9 +1,14 @@
 package com.example.gymtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.ListMenuItemView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toolbar;
 
 import java.util.ArrayList;
@@ -12,16 +17,36 @@ import java.util.List;
 import java.util.Map;
 
 public class ExercisesActivity extends AppCompatActivity {
-    Toolbar toolbar;
     ListView listView;
     List<String> names = new ArrayList<>();
     List<Integer> pictures = new ArrayList<>();
+    Map<String,Integer> chosenExercises = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercises);
 
+        initNamesList();
+        initPicturesList();
+
+        listView = findViewById(R.id.listView);
+        ListViewAdapter listViewAdapter = new ListViewAdapter(ExercisesActivity.this, names, pictures);
+        listView.setAdapter(listViewAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Integer chosenPicture = pictures.get(i);
+                String chosenName = names.get(i);
+
+
+
+            }
+        });
+    }
+
+    private void initNamesList(){
         names.add("Treadmill");
         names.add("Leg Press");
         names.add("Leg Curl");
@@ -32,7 +57,9 @@ public class ExercisesActivity extends AppCompatActivity {
         names.add("Arm Adduction");
         names.add("Arm Curl");
         names.add("Arm Extension");
+    }
 
+    private void initPicturesList(){
         pictures.add(R.drawable.treadmill);
         pictures.add(R.drawable.leg_press);
         pictures.add(R.drawable.leg_curl);
@@ -43,10 +70,5 @@ public class ExercisesActivity extends AppCompatActivity {
         pictures.add(R.drawable.arm_adduction);
         pictures.add(R.drawable.arm_curl);
         pictures.add(R.drawable.arm_extension);
-
-        listView = findViewById(R.id.listView);
-
-        ListViewAdapter listViewAdapter = new ListViewAdapter(ExercisesActivity.this, names, pictures);
-        listView.setAdapter(listViewAdapter);
     }
 }
